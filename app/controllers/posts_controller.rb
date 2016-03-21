564dd503f2edeaddd435b903a6fcadbb7b1535fd
Abdul Rahimi <afr114@gmail.com>
2016-03-21 12:59:02 -0700

    add initial files and posts controller

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,27 @@
+class PostsController < ApplicationController
+
+  def index
+  end
+
+  def create
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destory
+  end
+  
+end
