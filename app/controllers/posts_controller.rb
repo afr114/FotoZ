224409fb267520c069d651aa1a30ad2f7c7ebd41
Abdul Rahimi <afr114@gmail.com>
2016-03-21 21:33:43 -0700

    pass first capybara test

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,19 +1,29 @@
 class PostsController < ApplicationController
 
   def index
-  end
-
-  def create
+    @post = Post.all
   end
 
   def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.create(post_params)
+    if @post.save
+      redirect_to @post
+    else
+      render "new"
+    end
+  end
+
+  def show
+    @post = Post.find(params[:id])
   end
 
   def edit
   end
 
-  def show
-  end
 
   def edit
   end
@@ -23,5 +33,11 @@ class PostsController < ApplicationController
 
   def destory
   end
-  
+
+  private
+
+  def post_params
+    params.require(:post).permit(:image, :caption)
+  end
+
 end
